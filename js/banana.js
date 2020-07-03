00160c02de20214cc266b001b309faf58fef9010
gdl-888 <55873929+gdl-888@users.noreply.github.com>
2020-07-03 21:58:45 +0900

    토론 구현

--- js/banana.js
+++ js/banana.js
@@ -1,4 +1,36 @@
+/*
+  참고한 싸이트
+  - https://stackoverflow.com/questions/123999/how-can-i-tell-if-a-dom-element-is-visible-in-the-current-viewport
+*/
+
 $(function() {
+	function nevermind() {
+		return null;
+	}
+	
+	function isVisible(elmt) {
+		var top = elmt.offsetTop;
+		var left = elmt.offsetLeft;
+		const width = elmt.offsetWidth;
+		const height = elmt.offsetHeight;
+
+		while(elmt.offsetParent) {
+			elmt = elmt.offsetParent;
+			top += elmt.offsetTop;
+			left += elmt.offsetLeft;
+		}
+
+		return (
+			top < (window.pageYOffset + window.innerHeight) &&
+			left < (window.pageXOffset + window.innerWidth) &&
+			(top + height) > window.pageYOffset &&
+			(left + width) > window.pageXOffset
+		);
+	}
+	
+	const itoa = String;
+	const atoi = Number;
+	
 	$('.input-examples').on('change click', function() {
 		$(this).prev().val($(this).val());
 	});
@@ -62,4 +94,124 @@ $(function() {
 			}
 		});
 	});
+	
+	/* theseed.js 참고 안하고 처음부터 작성 */
+	/* 그런데 어째 이게 더 느린 느낌이지 */
+	const allLoadingRes = 'div.res-wrapper.res-loading[data-locked="false"]';
+	const loadingRes = 'div.res-wrapper.res-loading[data-visible="true"][data-locked="false"]';
+	
+	function setVisibleState() {
+		$(allLoadingRes).each(function() {
+			const item = $(this);
+			if(isVisible(item[0])) {
+				item.attr('data-visible', 'true');
+			} else {
+				item.attr('data-visible', 'false');
+			}
+		});
+	}
+	
+	document.addEventListener("scroll", setVisibleState);
+	
+	function discussFetch(tnum) {
+		setVisibleState();
+		
+		$(loadingRes).each(function() {
+			const item = $(this);
+
+			if(item.attr('data-locked') != 'true') {
+				item.attr('data-locked', 'true');
+				
+				$.ajax({
+					type: "GET",
+					url: '/thread/' + tnum + '/' + item.attr('data-id'),
+					dataType: 'html',
+					success: function(d) {
+						const data = $(d);
+						
+						data.each(function() {
+							const itm = $(this);
+							const res = $('div.res-wrapper.res-loading[data-id="' + itm.attr('data-id') + '"]');
+							
+							res.after(itm);
+							res.remove();
+						});
+					},
+					error: nevermind
+				});
+			}
+		});
+	}
+	
+	function discussPollStart(tnum) {
+		$('form#new-thread-form').submit(function() {
+			var submitBtn = $('form#new-thread-form').find('button[type="submit"]');
+			submitBtn.attr('disabled', '');
+			
+			$.ajax({
+				type: "POST",
+				dataType: 'json',
+				data: {
+					'text': $('textarea[name="text"]').val()
+				},
+				success: function(d) {
+					submitBtn.removeAttr('disabled');
+					$('textarea[name="text"]').val('');
+					
+					const data = $(d);
+					
+					data.each(function() {
+						const itm = $(this);
+						const res = $('div.res-wrapper.res-loading[data-id="' + itm.attr('data-id') + '"]');
+						
+						res.after(itm);
+						res.remove();
+					});
+				},
+				error: function(d) {
+					submitBtn.removeAttr('disabled');
+					
+					alert('댓글을 달 수 없습니다.');
+				}
+			});
+			
+			return false;
+		});
+		
+		var refresher = setInterval(function() {
+			$.ajax({
+				type: "POST",
+				url: '/notify/thread/' + tnum,
+				data: {},
+				dataType: 'json',
+				success: function(data) {
+					const tid = atoi(data['comment_id']);
+					var rescount = $('div.res-wrapper').length;
+					
+					for(var i=rescount+1; i<=tid; i++, rescount++) {
+						$('div.res-wrapper[data-id="' + itoa(rescount) + '"]').after($(
+							'<div class="res-wrapper res-loading" data-id="' + itoa(i) + '" data-locked="false" data-visible=false>' +
+								'<div class="res res-type-normal">' +
+									'<div class="r-head">' + 
+										'<span class="num"><a id="' + itoa(i) + '">#' + itoa(i) + '</a>&nbsp;</span>' +
+									'</div>' +
+									'' +
+									'<div class="r-body"></div>' +
+								'</div>' +
+							'</div>'
+						));
+					}
+					
+					setVisibleState();
+				},
+				error: nevermind
+			});
+			
+			discussFetch(tnum);
+		}, 2000);
+		
+		setVisibleState();
+	}
+	
+	window.discussPollStart = discussPollStart;
 });
