e05612a4a2295b61610faffc551008148ae195cc
gdl-888 <55873929+gdl-888@users.noreply.github.com>
2020-07-22 23:39:46 +0900

    윈도우 10을 윈도우 7 이전 운영체제로 인식하는 오류 수정

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 if(os.platform() == 'win32') {
     ver = os.release().split('.');
     
-    if( !( Number(ver[0]) >= 6 && Number(ver[1]) >= 1 ) ) {
+    if( !( Number(ver[0]) >= 6 && Number(ver[1]) >= 1 ) && ver[0] != '10' ) {
         console.log('이런! 이 애플리케이션은 Windows 7 이상의 환경(Node.js 6 이상)에서 호스팅할 수 있습니다 ^^;; 이용에 불편을 드려 대단히 죄송합니다 -;');
         process.exit(0);
     }
