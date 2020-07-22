d3f97bdcde7f2e8765727906bee63945702e76d6
gdl-888 <55873929+gdl-888@users.noreply.github.com>
2020-07-22 14:11:17 +0900

    -

--- server.js
+++ server.js
@@ -2,20 +2,25 @@ const os = require('os');
 const fs = require('fs');
 const vm = require('vm');
 
-if(Number(process.version.replace(/^v/, '').split('.')[0]) < 6) {
-	console.log('Node.js 버전 6 이상에서 실행해주세요~ ;;');
-	process.exit(0);
-}
-
 if(os.platform() == 'win32') {
 	ver = os.release().split('.');
 	
 	if(Number(ver[0]) >= 6 && Number(ver[1]) >= 1) {
+		if(Number(process.version.replace(/^v/, '').split('.')[0]) < 6) {
+			console.log('Node.js 버전 6 이상에서 실행해주세요~ ;;');
+			process.exit(0);
+		}
+		
 		require('./banana.js').run();
 	} else {
 		console.log('이런! Windows XP를 사용해주셔서 감사합니다만 이 애플리케이션은 Windows 7 이상의 환경(Node.js 6 이상)에서 호스팅할 수 있습니다 ^^;; 이용에 불편을 드려 대단히 죄송합니다 -;');
 		process.exit(0);
 	}
 } else {
+	if(Number(process.version.replace(/^v/, '').split('.')[0]) < 6) {
+		console.log('Node.js 버전 6 이상에서 실행해주세요~ ;;');
+		process.exit(0);
+	}
+
 	require('./banana.js').run();
 }
