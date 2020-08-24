1d6d36a290603b66d17527c0773386fb02f8e3e2
gdl-888 <gnote8080@gmail.com>
2020-08-24 21:39:59 +0900

    문서함 분류 만들기 기능 구현

--- routes/star.js
+++ routes/star.js
@@ -32,10 +32,12 @@ wiki.get(/\/member\/unstar\/(.*)/, async (req, res) => {
 		return res.send(await showError(req, 'not_starred'));
 	}
 	
+	/*
 	var dbdata = await curs.execute("select time from history where title = ? order by cast(rev as integer) desc limit 1", [title]);
 	if(!dbdata.length) {
 		return res.send(await showError(req, 'document_not_found'));
 	}
+	*/
 	
 	await curs.execute('delete from stars where title = ? and username = ?', [title, ip_check(req)]);
 
@@ -63,12 +65,12 @@ wiki.get('/member/starred_documents', async (req, res) => {
 		
 		navbar += '<li><a href="/member/starred_documents?category=' + encodeURIComponent(cate.name) + '">[' + html.escape(cate.name) + ']</a></li>';
 		
-		for(doc of (await curs.execute("select title, lastedit from stars where username = ? and category = ?", [ip_check(req), cate['name']]))) {
+		for(doc of (await curs.execute("select title, lastedit from stars where username = ? and category = ? order by cast(lastedit as integer) desc", [ip_check(req), cate['name']]))) {
 			content += `
 				<li>
 					${generateTime(toDate(doc.lastedit), 'm월 d일 h시 i분')}에 수정됨 - <a href="${encodeURIComponent(doc.title)}">${html.escape(doc.title)}</a>
 					<a href="/member/starred_documents/categorize?document=${encodeURIComponent(doc.title)}"> [분류 이동]</a>
-					<a href="/member/starred_documents/remove?document=${encodeURIComponent(doc.title)}"> [제거]</a>
+					<a href="/member/unstar/${encodeURIComponent(doc.title)}"> [제거]</a>
 				</li>
 			`;
 		}
@@ -79,4 +81,81 @@ wiki.get('/member/starred_documents', async (req, res) => {
 	navbar += '</ol>';
 
 	res.send(await render(req, '주시문서 목록', navbar + content));
+});
+
+wiki.get('/member/starred_documents/categories', async (req, res) => {
+	if(!islogin(req)) return res.redirect('/member/login?redirect=' + encodeURIComponent('/member/starred_documents'));
+	
+	// 간단히 정보만 쓰면 HTML 만들어주는 마법사 하나 만들까..
+	var content = `
+		<form method=post class=settings-section>
+			<div class=form-group>
+				<label>분류 이름:</label><br>
+				<input type=text class=form-control name=category id=categoryInput>
+			</div>
+			
+			<div class=btns>
+				<button type=submit class="btn btn-info" style="width: 100px;">추가</button>
+			</div>
+		</form>
+		
+		<table class=table>
+			<colgroup>
+				<col>
+				<col style="width: 80px;">
+			</colgroup>
+			
+			<thead>
+				<tr>
+					<th>이름</th>
+					<th>삭제</th>
+				</tr>
+			</thead>
+			
+			<tbody>
+	`;
+	
+	var dbdata = await curs.execute("select name from star_categories where username = ?", [ip_check(req)]);
+	
+	for(cate of dbdata) {
+		content += `
+			<tr>
+				<td>${html.escape(cate.name)}</td>
+				<td>
+					<form>
+						<input type=hidden name=category value="${html.escape(cate.name)}">
+						<input type=hidden name=submittype value=delete>
+						<button type=submit class="btn btn-danger btn-sm">삭제</button>
+					</form>
+				</td>
+			</tr>
+		`;
+	}
+	
+	content += '</tbody></table>';
+	
+	res.send(await render(req, '분류 추가', content));
+});
+
+wiki.post('/member/starred_documents/categories', async (req, res) => {
+	if(!islogin(req)) return res.redirect('/member/login?redirect=' + encodeURIComponent('/member/starred_documents'));
+	
+	if(req.body['category'] == '분류되지 않은 문서') {
+		return res.send(await showError(req, 'invalid_category_name'));
+	}
+	
+	if(req.body['submittype'] == 'delete') {
+		await curs.execute("delete from star_categories where username = ? and name = ?", [ip_check(req), req.body['category']]);
+	} else {
+		if((await curs.execute("select name from star_categories where username = ? and name = ?", [ip_check(req), req.body['category']])).length) {
+			return res.send(await showError(req, 'category_exists'));
+		}
+		if((await curs.execute("select name from star_categories where username = ?", [ip_check(req)])).length > 120) {
+			return res.send(await showError(req, 'category_overflow'));
+		}
+		
+		await curs.execute("insert into star_categories (name, username) values (?, ?)", [req.body['category'], ip_check(req)]);
+	}
+	
+	res.redirect('/member/starred_documents/categories');
 });
