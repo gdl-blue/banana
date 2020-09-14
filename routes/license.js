b90adfb32988c1409b0ff7e5df31131452ca364f
gdl-888 <gnote8080@gmail.com>
2020-09-14 14:39:18 +0900

    라이선스

---
+++ routes/license.js
@@ -0,0 +1,15 @@
+wiki.get('/License', async function(req, res) {
+	var sl = '';
+	try {
+		sl = await readFile('./skins/' + getSkins(req) + '/license.html');
+	} catch(e) {
+		sl = '(스킨 라이선스를 불러올 수 없습니다.)';
+	}
+
+	res.send(await render(req, '바나나 정보', `
+		<h2>바나나 ${versionInfo.major}.${versionInfo.minor}.${versionInfo.revesion}</h2>
+		<p>임시적으로 GNU 일반 공중 사용 허가서 버전 3을 사용합니다.</p>
+		
+		${sl}
+	`));
+});
